5ddd969be1cd95fcbbd6a520b2e2e39a82a5817c
RanierDalton <ranier.couto@sptech.school>
2025-11-20 11:35:41 -0300

    add: Adicionando propriedade de logo para o componente Header

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,12 @@
 import Header from "./Header";
 import Hero from "./Hero";
 import styles from '../styles/Home.module.css';
+import logo from '../assets/react.svg';
 
 export default function Home() {
     return (
         <div className={styles.container}>
-            <Header />
+            <Header logoSrc={logo} />
             <Hero />
         </div>
     );
